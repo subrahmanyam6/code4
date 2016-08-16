5294f54ab7f4f8abb6f55911db67240180f1bca7
subrahmanyam6 <bmdsubrahmanyam@gmail.com>
2016-08-16 23:23:50 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter n value");
+scanf("%d",&n);
+int a[n],i;
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+ if(i==a[i])
+ printf("%d",a[i]);
+ }
+ getch();
+ return 0;
+ }
